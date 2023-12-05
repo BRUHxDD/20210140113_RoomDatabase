a1c4c35e1619e0c094bc924f1c411eab2d3e9048
Teuku Muhammad Attar Fadhilah <attarku05@gmail.com>
2023-12-05 23:24:08 +0700

    PenyediaViewModel.kt file finished

--- app/src/main/java/com/example/minggu10/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/minggu10/model/PenyediaViewModel.kt
@@ -1,2 +1,25 @@
 package com.example.minggu10.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.minggu10.AplikasiSiswa
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+/**
+ * Fungsi ekstensi query untuk objek [application] dan mengembalikan sebuah instance dari
+ * [AplikasiSiswa].
+ */
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
