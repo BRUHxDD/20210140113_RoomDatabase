98b07e74e5f5d7e662eb4b8ecf1c0630ebc8a7f3
Teuku Muhammad Attar Fadhilah <attarku05@gmail.com>
2023-12-06 20:45:50 +0700

    Add BodyHome function at HalamanHome.kt file

--- app/src/main/java/com/example/minggu10/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/minggu10/ui/halaman/HalamanHome.kt
@@ -1,9 +1,17 @@
 package com.example.minggu10.ui.halaman
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.minggu10.R
+import com.example.minggu10.data.Siswa
 import com.example.minggu10.model.HomeViewModel
 import com.example.minggu10.model.PenyediaViewModel
 
@@ -13,4 +21,26 @@ fun HomeScreen(
     navigateToItemEntry: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){}
+) {
+}
+
+@Composable
+fun BodyHome(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        if (itemSiswa.isEmpty()) {
+            Text(
+                text = stringResource(R.string.deskripsi_no_item),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge
+            )
+        } else {
+
+        }
+    }
+}
