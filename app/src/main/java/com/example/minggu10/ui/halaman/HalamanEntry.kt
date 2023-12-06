4f432f39c11e98c17cbb2c8f3c0379ad0b401ddb
Teuku Muhammad Attar Fadhilah <attarku05@gmail.com>
2023-12-06 19:45:09 +0700

    Add EntrySiswaScreen function

--- app/src/main/java/com/example/minggu10/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/minggu10/ui/halaman/HalamanEntry.kt
@@ -1,2 +1,29 @@
 package com.example.minggu10.ui.halaman
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.minggu10.R
+import com.example.minggu10.model.EntryViewModel
+import com.example.minggu10.model.PenyediaViewModel
+import kotlinx.coroutines.launch
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+}
