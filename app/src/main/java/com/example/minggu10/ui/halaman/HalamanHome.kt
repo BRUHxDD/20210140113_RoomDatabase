9f12126869fe611334622f936970807e306f0211
Teuku Muhammad Attar Fadhilah <attarku05@gmail.com>
2023-12-06 20:35:32 +0700

    Add HomeScreen function at HalamanHome.kt file

--- app/src/main/java/com/example/minggu10/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/minggu10/ui/halaman/HalamanHome.kt
@@ -1,2 +1,16 @@
 package com.example.minggu10.ui.halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.minggu10.model.HomeViewModel
+import com.example.minggu10.model.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){}
