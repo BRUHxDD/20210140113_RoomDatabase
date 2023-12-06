6d5d6896765451255da0825bc0577f75add84e48
Teuku Muhammad Attar Fadhilah <attarku05@gmail.com>
2023-12-06 21:32:28 +0700

    Add HostNavigasi function at PengelolaHalaman.kt file

--- app/src/main/java/com/example/minggu10/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/minggu10/navigasi/PengelolaHalaman.kt
@@ -12,20 +12,31 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.minggu10.R
+import com.example.minggu10.ui.halaman.DestinasiEntry
+import com.example.minggu10.ui.halaman.DestinasiHome
+import com.example.minggu10.ui.halaman.EntrySiswaScreen
+import com.example.minggu10.ui.halaman.HomeScreen
 
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController()){
+    HostNavigasi(navController = navController)
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SiswaTopAppBar(
-    title:String,
+    title: String,
     canNavigateBack: Boolean,
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {}
-){
-    CenterAlignedTopAppBar(title = { Text(title)},
+) {
+    CenterAlignedTopAppBar(title = { Text(title) },
 
         modifier = modifier,
         scrollBehavior = scrollBehavior,
@@ -41,4 +52,26 @@ fun SiswaTopAppBar(
             }
         }
     )
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(
+        navController=navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiEntry.route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = {navController.popBackStack()})
+        }
+    }
 }
